0bb8bfd2f6b15d9339a5082f4f00fe04258b9895
Andreyeva Sofiya <Sofiy17.07.04@gmail.com>
2025-04-27 16:40:39 +0300

    added a registration screen

---
+++ app/src/main/java/bsu/pi_13/flowers_team/feature/register/screen/RegisterScreen.kt
@@ -0,0 +1,46 @@
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import bsu.pi_13.flowers_team.feature.register.view_model.RegisterViewModel
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.NavController
+import bsu.pi_13.flowers_team.data.DatabaseHelper
+
+@Composable
+fun RegisterScreen(navController: NavController, dbHelper: DatabaseHelper) {
+    val context = LocalContext.current
+    val viewModel = remember { RegisterViewModel(dbHelper, context) }
+    val pinkBackground = Color(0xFFF8BBD0)
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(pinkBackground) 
+    )
+    {
+        RegisterContent(
+            login = viewModel.login,
+            password = viewModel.password,
+            errorMessage = viewModel.errorMessage,
+            isLoading = viewModel.isLoading,
+            onLoginChange = { viewModel.login = it },
+            onPasswordChange = { viewModel.password = it },
+            onRegisterClick = {
+                viewModel.registerUser {
+                    // Navigate to the main screen on successful registration
+                    navController.navigate("main_screen") {
+                        popUpTo("register_screen") { inclusive = true }
+                    }
+                }
+            },
+            onLoginNavigate = {
+                navController.navigate("login_screen") {
+                    popUpTo("register_screen") { inclusive = false }
+                }
+            }
+        )
+    }
+}
