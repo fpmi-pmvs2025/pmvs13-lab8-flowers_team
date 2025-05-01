c8bee6e5f011d665c0f2296a23e333ab56d6bf97
Andreyeva Sofiya <Sofiy17.07.04@gmail.com>
2025-05-01 18:51:37 +0300

    comments and imports have been deleted

--- app/src/main/java/bsu/pi_13/flowers_team/feature/basket/screen/BasketScreen.kt
+++ app/src/main/java/bsu/pi_13/flowers_team/feature/basket/screen/BasketScreen.kt
@@ -1,28 +1,12 @@
 package bsu.pi_13.flowers_team.feature.basket.screen
 
-import BasketItemCard
 import EmptyBasketView
-import NotificationBanner
 import NotificationView
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.slideInVertically
-import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import bsu.pi_13.flowers_team.data.db.DatabaseHelper
 import bsu.pi_13.flowers_team.data.model.Flower
@@ -30,7 +14,6 @@ import bsu.pi_13.flowers_team.feature.basket.content.BasketContent
 import bsu.pi_13.flowers_team.feature.basket.view_model.BasketViewModel
 import bsu.pi_13.flowers_team.ui.theme.*
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 @Composable
 fun BasketScreen(
