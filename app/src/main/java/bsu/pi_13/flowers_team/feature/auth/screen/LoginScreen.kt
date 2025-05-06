fe9a02a6345f63e5f3c0f279561e7cd4e037359d
Andreyeva Sofiya <Sofiy17.07.04@gmail.com>
2025-05-06 09:51:06 +0300

    fix

--- app/src/main/java/bsu/pi_13/flowers_team/feature/auth/screen/LoginScreen.kt
+++ app/src/main/java/bsu/pi_13/flowers_team/feature/auth/screen/LoginScreen.kt
@@ -1,9 +1,4 @@
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavController
 import bsu.pi_13.flowers_team.data.db.DatabaseHelper
@@ -16,28 +11,29 @@ fun LoginScreen(navController: NavController, dbHelper: DatabaseHelper) {
     val context = LocalContext.current
     val viewModel = remember { LoginViewModel(dbHelper, context) }
     val coroutineScope = rememberCoroutineScope()
-        LoginContent(
-            login = viewModel.login,
-            password = viewModel.password,
-            errorMessage = viewModel.errorMessage,
-            isLoading = viewModel.isLoading,
-            onLoginChange = { viewModel.login = it },
-            onPasswordChange = { viewModel.password = it },
-            onLoginClick = {
-                coroutineScope.launch {
-                    val isSuccess = viewModel.loginUser()
-                    if (isSuccess) {
-                        navController.navigate("main_screen") {
-                            popUpTo("login_screen") { inclusive = true }
-                        }
+
+    LoginContent(
+        login = viewModel.login,
+        password = viewModel.password,
+        errorMessage = viewModel.errorMessage,
+        isLoading = viewModel.isLoading,
+        onLoginChange = { viewModel.login = it },
+        onPasswordChange = { viewModel.password = it },
+        onLoginClick = {
+            coroutineScope.launch {
+                val isSuccess = viewModel.loginUser()
+                if (isSuccess) {
+                    navController.navigate("main_screen") {
+                        popUpTo("login_screen") { inclusive = true }
                     }
                 }
-            },
-            onRegisterClick = {
-                navController.navigate("register_screen") {
-                    popUpTo("login_screen") { inclusive = false }
-                }
             }
-        )
+        },
+        onRegisterClick = {
+            navController.navigate("register_screen") {
+                popUpTo("login_screen") { inclusive = false }
+            }
+        }
+    )
 
 }
